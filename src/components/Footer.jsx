69fe0fa38833413fc16af80f8c0d9b437a46c517
MuhammadYeasinArafath <mdyeasinarafathofficial@gmail.com>
2022-12-07 14:13:15 +0600

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,16 +4,7 @@ import "./css/Footer.css";
 const Footer = () => {
     return (
         <>
-            <footer className='footer m-3'>
-
-            <h4 className='mb-2'>Done Looking At My Portfolio?</h4>
-            <h5 className='mb-2'>Well, I Know Your Answer.</h5>
-            <h6 className='mb-2'>Here Is Another Part Of My Portfolio I Have Made With Wordpress, Where I Have Showed All My Projects Which I Have Done With CMS Like WordPress and Shopify.</h6>
-             <p className='mb-2'>You Will Also Get To know More Stuff About Me From There.</p>
-            <a href="http://www.yeasinarafathportfolio.xyz/built-with-wordpress/" target="_blank" rel="noopener noreferrer"><button className='btn btn-primary custom-btn ps-5 pe-5 fw-bold mt-2'>Click To Go</button></a>
-
-<br />
-<hr />
+            <footer className='footer'>
                 <small>&copy; Copyright 2022-Today for <span>Md. Yeasin Arafath Portfolio</span>.</small>
                 <br />
                  <small>Designed & Developed by <span>Md. Yeasin Arafath</span>.</small> 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,24 +6,28 @@ const Navbar = () => {
     <>
     <nav className="bg-light navbar navbar-expand-lg shadow-sm p-4">
       <div className="container-fluid d-sm-flex flex-column flex-lg-row justify-content-center justify-content-lg-between">
-        
+
         <div className="navbar-brand mb-4 mb-md-0 mb-lg-0">
           <img src={logo} alt="logo" width="30" height="auto" className="d-inline-block align-text-top" />
           <span className="ms-2 fw-bold">Md. Yeasin Arafath</span>
         </div>
         
-        <div className="">
+        <div className=" text-center">
           <NavLink to='/' className="link me-5" end>Home</NavLink>
           <NavLink to='/about' className="link me-5">About</NavLink>
           <NavLink to='/projects' className="link me-5">Projects</NavLink>
+          <br />
+          <br />
+          <a href="http://www.yeasinarafathportfolio.xyz/built-with-wordpress/" target="_blank" rel="noopener noreferrer" className="link me-5">WordPress Portfolio</a>
+          <a href="https://mdyeasinarafath.wixsite.com/wixportfolio" target="_blank" rel="noopener noreferrer" className="link me-5">Wix Portfolio</a>
+          <a href="https://developeryeasinarafath.blogspot.com/" target="_blank" rel="noopener noreferrer" className="link me-5">Blog</a>
         </div>
-        
         <div>
           <NavLink to='/contact' className="link">
             <button className='mt-5 m-lg-0 btn btn-primary custom-btn fw-bold'>Contact me</button>
           </NavLink>
         </div>
-
+        
       </div>
   </nav>
   </>
